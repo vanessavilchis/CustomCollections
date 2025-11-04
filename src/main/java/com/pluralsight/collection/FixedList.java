20de914fbb2881fb4ed4467802d8c9642975749c
vanessavilchis <vanessavilchis.info@gmail.com>
2025-11-04 11:24:08 -0500

    Done

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,26 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items; //hold list of items
+    private int maxSize;
+
+    public FixedList(int maxSize) {
+        this.maxSize = maxSize;
+        this.items = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        if (items.size() >= maxSize) {
+            System.out.println("Cannot add item. List has reached maximum size of " + maxSize);
+            return;
+        }
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
 }
